7814d9096f1600df59d70fdf535447faf587ee22
seha <hansaemj20@gmail.com>
2025-07-27 10:42:53 +0900

    lambda 배포 완

---
+++ src/lambda.ts
@@ -0,0 +1,27 @@
+// /src/lambda.ts
+import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
+
+import { AppModule } from './app.module';
+
+let server: Handler;
+
+async function bootstrap(): Promise<Handler> {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
+}
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  if (event.path === '' || event.path === undefined) event.path = '/';
+
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
